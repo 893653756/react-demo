5c6af43ec0c5c4d9db25572db9a625c3bb53c6a7
zhuwanhai <893653756@qq.com>
2020-01-20 13:08:30 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
 /**
  * 应用根组件
  */
 export default class App extends Component {
-    render () {
+    render() {
         return (
-            <Button type="primary">按钮</Button>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/admin" component={Admin}></Route>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
